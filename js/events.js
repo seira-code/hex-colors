ac41152663484e90a0b9fd979a0f9735b14d6843
seira-code <gubarevich.artem@mail.ru>
2025-04-25 13:35:14 +0300

    created

---
+++ js/events.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let localStrg = localStorage.getItem("sections")
+  if (!localStrg) localStrg = JSON.stringify(randomSections())
+
+  let storage = JSON.parse(localStrg)
+  if (Object.values(storage).length < 5) return randomSections()
+
+  loadColors(storage)
+})
+
+document.addEventListener("keydown", (event) => {
+  const key = event.key.toLowerCase()
+  console.log(key)
+  switch (key) {
+    case " ": // SPACE
+      randomSections()
+      break;
+    case "r":
+      randomSections(true) // reset
+      break;
+    case "к":
+      randomSections(true)
+      break;
+
+    default:
+      break;
+  }
+})
---
+++ js/index.js
@@ -0,0 +1,56 @@
+const randomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`
+
+function setColorsInSection(section, color, isLock, index) {
+  const sectionText = section.getElementsByTagName("p")[0]
+  const sectionLock = section.getElementsByTagName("img")[0]
+
+  const lumine = chroma(color).luminance() > 0.5 ? "black" : "white"
+
+  sectionText.innerHTML = color
+  sectionText.style.color = lumine
+
+  sectionLock.style.filter = lumine == "black" ? "brightness(0) invert(0)" : "brightness(0) invert(1)"
+  sectionLock.src = isLock ? "../assets/lock.svg" : "../assets/unlock.svg"
+  section.style.backgroundColor = color
+
+  sectionText.setAttribute("onclick", `handleTextClick("${color}")`)
+  sectionLock.setAttribute("onclick", `handleLockerClick(${index})`)
+}
+
+function randomSections(reset = false) {
+  let store = JSON.parse(localStorage.getItem("sections")) ?? {}
+  if (reset) store = {}
+
+  for (let i = 1; i < 6; i++) {
+    if (!reset && store[i]?.lock) continue;
+    const section = document.getElementById(`section${i}`)
+    const color = randomColor()
+
+    setColorsInSection(section, color, false, i)
+
+    store[i] = { color, lock: false }
+  }
+
+  localStorage.setItem("sections", JSON.stringify(store))
+  return store
+}
+
+function loadColors(store) {
+  const values = Object.values(store)
+  for (let i = 0; i < 5; i++) {
+    const section = document.getElementById(`section${i + 1}`)
+    setColorsInSection(section, values[i].color, values[i].lock, i + 1)
+  }
+}
+
+function handleLockerClick(index) {
+  const store = JSON.parse(localStorage.getItem("sections"))
+  store[index] = { ...store[index], lock: !store[index].lock }
+  localStorage.setItem("sections", JSON.stringify(store))
+  loadColors(store)
+}
+
+async function handleTextClick(text) {
+  await navigator.clipboard.writeText(text)
+  alert(`Copied: ${text}`)
+}
